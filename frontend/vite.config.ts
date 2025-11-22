cf4b43528d10cde4b6df2c83415c1ad7da8e96a9
kwojtas912@gmail.com <kwojtas912@gmail.com>
2025-11-22 10:42:49 +0100

    Added Docker build for backend and frontend.
    (cherry picked from commit bdb08c75982c1b35afa116b7ba92a64619161c07)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "fs";
-import path from "path";
 
 export default defineConfig({
     plugins: [react()],
     server: {
         https: {
-            key: fs.readFileSync(path.resolve(__dirname, "certs/localhost-key.pem")),
-            cert: fs.readFileSync(path.resolve(__dirname, "certs/localhost.pem")),
+            key: fs.readFileSync("/app/certs/localhost-key.pem"),
+            cert: fs.readFileSync("/app/certs/localhost.pem"),
         },
         port: 3000,
+        host: "0.0.0.0",
+    },
+    preview: {
+        https: {
+            key: fs.readFileSync("/app/certs/localhost-key.pem"),
+            cert: fs.readFileSync("/app/certs/localhost.pem"),
+        },
+        port: 4173,
         host: "0.0.0.0",
     },
 });
